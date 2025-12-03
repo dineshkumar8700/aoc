8a48964ecbb983373b38ff26362f585371aa79e8
Dinesh Kumar <179027812+dineshkumar8700@users.noreply.github.com>
2025-12-03 15:31:47 +0530

    Removed repetition of code

--- 02_program_alarm/src/program_alarm.js
+++ 02_program_alarm/src/program_alarm.js
@@ -1,19 +1,48 @@
 const data = Deno.readTextFileSync("./data/input.txt");
-
 const parsedData = data.split(",").map((element) => parseInt(element));
 
-const executeInstructions = (data) => {
-  for (let index = 0; index < data.length; index += 4) {
-    if (data[index] === 1) {
-      data[data[index + 3]] = data[data[index + 1]] + data[data[index + 2]];
-    } else if (data[index] === 2) {
-      data[data[index + 3]] = data[data[index + 1]] * data[data[index + 2]];
-    } else if (data[index] === 99) return data;
-  }
+const add = (x, y) => {
+  return x + y;
+};
+
+const multiply = (x, y) => {
+  return x * y;
+};
+
+const performOperation = (data, index, operation) => {
+  data[data[index + 3]] = operation(
+    data[data[index + 1]],
+    data[data[index + 2]],
+  );
 
   return data;
 };
 
-executeInstructions(parsedData);
+const executeIntcode = (input) => {
+  const data = [...input];
+  data[1] = 12;
+  data[2] = 2;
 
-console.log(parsedData[0]);
+  for (let index = 0; index < data.length; index += 4) {
+    if (data[index] === 1) performOperation(data, index, add);
+    else if (data[index] === 2) performOperation(data, index, multiply);
+    else if (data[index] === 99) return data[0];
+  }
+
+  return data[0];
+};
+
+const findNounAndVerb = (input, target) => {
+  const data = [...input];
+
+  for (let i = 0; i <= 99; i++) {
+    for (let j = 0; j <= 99; j++) {
+      data[1] = i;
+      data[2] = j;
+      if (executeIntcode(data) === target) return i + "" + j;
+    }
+  }
+};
+
+console.log(executeIntcode(parsedData));
+// console.log(findNounAndVerb(parsedData, 19690720));
