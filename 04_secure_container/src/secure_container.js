05541b67c5099620fd13e3949a008450fc47b5ca
Dinesh Kumar <179027812+dineshkumar8700@users.noreply.github.com>
2025-12-04 21:26:27 +0530

    Completed 4th day part 2

--- 04_secure_container/src/secure_container.js
+++ 04_secure_container/src/secure_container.js
@@ -3,29 +3,31 @@ const [lower, upper] = range.map((x) => parseInt(x));
 
 const isValidNumber = (number) => {
   const strNum = "" + number;
-  let areTwoDigitsSame = false;
+  const frequency = {};
 
-  for (let i = 0; i < strNum.length - 1; i++) {
+  for (let i = 0; i < strNum.length; i++) {
     if (parseInt(strNum[i]) > parseInt(strNum[i + 1])) return false;
 
-    if (strNum[i] === strNum[i + 1]) {
-      areTwoDigitsSame = true;
-    }
+    frequency[strNum[i]] = frequency[strNum[i]] ? frequency[strNum[i]] += 1 : 1;
   }
 
-  return areTwoDigitsSame;
+  for (const key in frequency) {
+    if (frequency[key] === 2) return true;
+  }
+
+  return false;
 };
 
 const getValidNumbers = (start, end) => {
-  const validPasswords = [];
+  const validNumbers = [];
 
   for (let number = start; number < end; number++) {
     if (isValidNumber(number)) {
-      validPasswords.push(number);
+      validNumbers.push(number);
     }
   }
 
-  return validPasswords;
+  return validNumbers;
 };
 
 const validPass = getValidNumbers(lower, upper);
