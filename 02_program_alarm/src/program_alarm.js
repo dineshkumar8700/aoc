a7d1667f25337c9cc6c367aff3f6ce98e0499962
Dinesh Kumar <179027812+dineshkumar8700@users.noreply.github.com>
2025-12-19 06:42:14 +0530

    Optimised code for 2nd day problem 1

--- 02_program_alarm/src/program_alarm.js
+++ 02_program_alarm/src/program_alarm.js
@@ -1,46 +1,65 @@
 const data = Deno.readTextFileSync("./data/input.txt");
 const parsedData = data.split(",").map((element) => parseInt(element));
 
+const OPERATIONS = {
+  1: (memory, ip) => {
+    updateMemory(memory, ip, add);
+    return ip + 4;
+  },
+  2: (memory, ip) => {
+    updateMemory(memory, ip, multiply);
+    return ip + 4;
+  },
+  99: (memory, ip) => {
+    return null;
+  },
+};
+
 const add = (x, y) => x + y;
 
 const multiply = (x, y) => x * y;
 
-const performOperation = (data, index, operation) => {
-  data[data[index + 3]] = operation(
-    data[data[index + 1]],
-    data[data[index + 2]],
-  );
+const updateMemory = (memory, ip, operation) => {
+  const input1Pos = memory[ip + 1];
+  const input2Pos = memory[ip + 2];
+  const outputPos = memory[ip + 3];
 
-  return data;
+  memory[outputPos] = operation(memory[input1Pos], memory[input2Pos]);
 };
 
-const executeIntcode = (input, part = 2) => {
-  const data = [...input];
-  if (part === 1) {
-    data[1] = 12;
-    data[2] = 2;
-  }
-
-  for (let index = 0; index < data.length; index += 4) {
-    if (data[index] === 1) performOperation(data, index, add);
-    else if (data[index] === 2) performOperation(data, index, multiply);
-    else if (data[index] === 99) return data[0];
-  }
-
-  return data[0];
+const applyOverrides = (memory, overrides) => {
+  overrides.forEach(([position, value]) => {
+    memory[position] = value;
+  });
 };
 
-const findNounAndVerb = (input, target) => {
-  const data = [...input];
+const executeIntcode = (input, overrides = []) => {
+  const memory = [...input];
+  applyOverrides(memory, overrides);
 
-  for (let i = 0; i <= 99; i++) {
-    for (let j = 0; j <= 99; j++) {
-      data[1] = i;
-      data[2] = j;
-      if (executeIntcode(data) === target) return i + "" + j;
-    }
+  let ip = 0;
+
+  while (ip !== null) {
+    const opcode = memory[ip];
+    const operation = OPERATIONS[opcode];
+    ip = operation(memory, ip);
   }
+
+  return memory[0];
 };
 
-console.log(executeIntcode(parsedData, 1));
-console.log(findNounAndVerb(parsedData, 19690720));
+console.log(executeIntcode(parsedData, [[1, 12], [2, 2]]));
+
+// console.log(findNounAndVerb(parsedData, 19690720));
+
+// const findNounAndVerb = (input, target) => {
+//   const data = [...input];
+
+//   for (let i = 0; i <= 99; i++) {
+//     for (let j = 0; j <= 99; j++) {
+//       data[1] = i;
+//       data[2] = j;
+//       if (executeIntcode(data) === target) return i + "" + j;
+//     }
+//   }
+// };
